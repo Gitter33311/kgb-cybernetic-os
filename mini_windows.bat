e8ac9fa8c993e53878d3385232e62a4411ed63e5
Gitter33311 <szabi1234567891011@gmail.com>
2026-09-10 21:11:58 +0200

    Add files via upload

--- mini_windows.bat
+++ mini_windows.bat
@@ -1,41 +1,49 @@
 @echo off
-:: Szovjet vörös háttér, sárga betûk a retro hangulathoz
+:: Szovjet vörös háttér, sárga betûk
 color 4e
-title KGB Mini-Windows OS v1.0
+title KGB Cybernetic OS v3.0
 
 :BOOT
 cls
-echo [========================================]
-echo        KGB CYBERNETIC OS LOADING...
-echo [========================================]
+echo ====================================================================
+echo   ____  _____ ____    ____  _   _ _   _ _  _ _____ _____ ___ ____ 
+echo  / ___^| ____|  _ \  | __ )^| ^| ^| ^| \ ^| ^| ^|/ ^| ____|_   _|_ _/ ___^|
+echo ^| ^|   ^|  _^| ^| ^|_) ^| ^|  _ \^| ^| ^| ^|  \^| ^| ' /^|  _|   ^| ^|  ^| ^| ^|    
+echo ^| ^|___^| ^|___^|  _ <  ^| ^|_) ^| ^|_^| ^| ^|\  ^| . \^| ^|___  ^| ^|  ^| ^| ^|___ 
+echo  \____^|_____^|_^| \_\ ^|____/ \___/^|_^| \_^|_^|\_\_____^| ^|_^| ^|___\____^|
+echo ====================================================================
+echo.
+echo           --- KIBERNETIKUS RENDSZER BETÖLTÉSE ---
 echo Rendszerfajlok ellenorzese...
-timeout /t 2 >nul
-echo Periferiak csatlakoztatasa...
 timeout /t 1 >nul
-echo Rendszer kesz.
+echo KGB titkos hálózati protokollok élesítése...
+timeout /t 1 >nul
+echo Elvtárs azonosítva. Rendszer kész.
 timeout /t 1 >nul
 
 :DESKTOP
 cls
 echo ====================================================================
-echo   KGB MINI-WINDOWS   ^|   Ido: %time:~0,5%   ^|   Felhasznalo: ELVTARS
+echo   KGB CYBERNETIC OS   ^|   Idõ: %time:~0,5%   ^|   Felhasználó: ELVTÁRS
 echo ====================================================================
 echo.
-echo     [1] KGB FILE EXPLORER (Fajlkezelo)
+echo     [1] KGB FILE EXPLORER (Fájlkezelõ)
 echo     [2] RED TEXT EDITOR (Mini Notepad)
 echo     [3] SYSTEM TERMINAL (Parancssor)
-echo     [4] SYSTEM INFO (Rendszerinformacio)
-echo     [5] SHUTDOWN (Kilepes/Leallitas)
+echo     [4] SYSTEM INFO (Rendszerinformáció)
+echo     [5] CYBER-GAME (Szovjet Mini-Játék)
+echo     [6] SHUTDOWN (Kilépés a rendszerbõl)
 echo.
 echo ====================================================================
 echo.
-set /p "task=Valasszon egy alkalmazast (1-5): "
+set /p "task=Valasszon egy alkalmazast (1-6): "
 
 if "%task%"=="1" goto EXPLORER
 if "%task%"=="2" goto NOTEPAD
 if "%task%"=="3" goto TERMINAL
 if "%task%"=="4" goto SYSINFO
-if "%task%"=="5" exit
+if "%task%"=="5" goto GAME
+if "%task%"=="6" exit
 goto DESKTOP
 
 :EXPLORER
@@ -55,7 +63,7 @@ goto EXPLORER
 :NOTEPAD
 cls
 echo === RED TEXT EDITOR ===
-echo Irja be a menteni kivant szöveget (Irat nevû fajlba menti):
+echo Irja be a menteni kivant szöveget (KGB_irat.txt-be menti):
 echo --------------------------------------------------------------------
 set /p "text=> "
 echo %text% > kgb_irat.txt
@@ -80,10 +88,41 @@ goto term_loop
 cls
 echo === KGB SYSTEM INFO ===
 echo.
-echo Operacios rendszer: KGB Mini-Windows v1.0
+echo Operacios rendszer: KGB Cybernetic OS v3.0
 echo Alap rendszer: Windows 7 Core
 echo Szamitogep neve: %computername%
 echo Logikai processzorok szama: %NUMBER_OF_PROCESSORS%
 echo.
+pause
+goto DESKTOP
+
+:GAME
+cls
+:: Generálunk egy véletlen számot 1 és 50 között
+set /a "titkosszam=%random% %% 50 + 1"
+set "proba=0"
+echo ====================================================================
+echo                 SZOVJET CYBER-JÁTÉK: TALÁLD MEG A KÓDOT!
+echo ====================================================================
+echo A gép gondolt egy számra 1 és 50 között. Találd ki!
+echo.
+
+:game_loop
+set /p "tipp=Adja meg a tippet: "
+set /a "proba+=1"
+
+if %tipp% equ %titkosszam% goto game_win
+if %tipp% blt %titkosszam% echo [RENDSZER]: A titkos szám NAGYOBB!
+if %tipp% bgt %titkosszam% echo [RENDSZER]: A titkos szám KISEBB!
+echo.
+goto game_loop
+
+:game_win
+echo.
+echo ====================================================================
+echo   GRATULÁLUNK ELVTÁRS! Kitaláltad a számot (%titkosszam%)!
+echo   Próbálkozások száma: %proba%
+echo   A Párt büszke Önre!
+echo ====================================================================
 pause
 goto DESKTOP
